8f014e165f339cbf4d9320b10639b4682413c0e2
7omahawk <ahulr.official@gmial.com>
2024-04-09 02:32:29 +0600

    adding decryption method and choice method and also changing some methodology

--- multiplicativeCipher.py
+++ multiplicativeCipher.py
@@ -1,45 +1,107 @@
 # making multiplicative cipher encryption and decryption using python
 
+import sys
 
 domain = 26
 string = "abcdefghijklmnopqrstuvwxyz"
 
+# making multiplicative inverse
+def multiplicativeInverse(key, domain):
 
-userInput = input("Enter your text to encrypt: ")
-key = int(input("Enter the key: "))
-userInput = userInput.lower()
+    r2 = key
+    r1 = domain
+    Q = ''
+    flag = 0
 
-# def encryption(userInput, key, domain, string):
+    for i in range(r1):       
+        q = int(r1/r2) 
+        Q = Q + str(q)
+        r = r1%r2 
+
+        temp1 = r2
+        r1 = temp1 #r2-------->r1
+        temp2 = r 
+        r2 = temp2 #r-------->r2
     
-#     cipher = ""
-#     for i in range(len(userInput)):
-#         for j in range(len(string)):
-#             if string[j] == userInput[i]:
-#                 text = (string[(j*key)%domain])
-#                 cipher = cipher + text
+        flag += 1
+
+        if r2 ==0:
+            break
+
+    if r1 == 1: 
+        t1 = 0
+        t2 = 1
+        q = int(str(Q)[::-1])   #inverse the value of q by string slicing reverse method
+
+        for i in range(flag):
+            fakeQ = q
+            q = fakeQ % 10
+            t = t1 - (t2 * q) #the main equation
+            q = int(fakeQ / 10)
+
+            temp1 = t2
+            t1 = temp1 #t2-------->t1
+            temp2 = t
+            t2 = temp2 #t-------->t2
+
+        if t1 < 0:
+            t1 = t1 + domain 
+    else:
+        print("For this key there will be no multiplicative inverse.")
+    return t1
+
+# encryption method
+def encryption(userInput, key, domain, string):
+    
+    cipher = ""
+    for i in range(len(userInput)):
+        for j in range(len(string)):
+            if string[j] == userInput[i]:
+                text = (string[(j*key)%domain])
+                cipher = cipher + text
                 
-#     print(f"The encrypted message is: {cipher}")
-#     print('\n')
+    print(f"The encrypted message is: {cipher}")
+    print('\n')
 
-# encryption(userInput, key, domain, string)
+# decryption method
+def decryption(userInput, key, t1, domain, string):
+    
+    multiplicativeInverse(key, domain)
 
+    cipher = ""
+    for i in range(len(userInput)):
+        for j in range(len(string)):
+            if string[j] == userInput[i]:
+                text = (string[(j*t1)%domain])
+                cipher = cipher + text
+                
+    print(f"The encrypted message is: {cipher}")
+    print('\n')
 
-r2 = key
-r1 = domain
-Q = ''
+# choice the what you need to do (encrypt/decrypt)
+while(True):
+    print("Enter your choice(Number): ")
+    print("1. Encryption: ")
+    print("2. Decryption: ")
+    print("3. Exit: ")
 
-for i in range(r1):       
-    q = int(r1/r2) 
-    Q = Q + str(q)
-    r = r1%r2 
+    number = int(input("Enter the number: "))
 
-    temp1 = r2
-    r1 = temp1 #r2-------->r1
-    temp2 = r 
-    r2 = temp2 #r2-------->r
+    def choice(number):
+        if number == 1:
+            userInput = input("Enter your text to encrypt: ")
+            key = int(input("Enter the key: "))
+            userInput = userInput.lower()
+            encryption(userInput, key, domain, string)
+        elif number == 2:
+            userInput = input("Enter your text to decrypt: ")
+            key = int(input("Enter the key: "))
+            userInput = userInput.lower()
+            t1 = multiplicativeInverse(key, domain)
+            decryption(userInput, key, t1, domain, string)
+        elif number == 3:
+            sys.exit()
+        else:
+            print("Input should be a number from 1 to 3")
 
-    if r2 ==0:
-        break
-
-print(Q)
-print(r1)
+    choice(number) 
